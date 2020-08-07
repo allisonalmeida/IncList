563d0500f6f540caed52a2d132995b9ef9c3d3c8
allisonalmeida <allisodealmeida@gmail.com>
2020-08-06 22:05:23 -0300

    backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+const {errors} = require('celebrate');
 
 const routes = require('./routes');
 
@@ -14,5 +15,6 @@ app.use(helmet());
 app.use(morgan('tiny'));
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
 app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const {celebrate, Joi, Segments} = require('celebrate');
 
 const UserController = require('./controllers/UserController');
 const IncidentController = require('./controllers/IncidentController');
@@ -8,24 +9,59 @@ const SessionController = require('./controllers/SessionController');
 const routes = express.Router();
 
 // route for session creation
-routes.post('/sessions', SessionController.create);
+routes.post('/sessions', celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    id: Joi.string().required().length(8),
+  })
+}), SessionController.create);
 
 // route to list users
 routes.get('/users', UserController.index);
 
 // user creation route
-routes.post('/users', UserController.create);
+routes.post('/users', celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    name: Joi.string().required().required(),
+    email: Joi.string().lowercase().email().required(),
+    whatsapp: Joi.string().min(10).max(11).required(),
+    city: Joi.string().required(),
+    uf: Joi.string().length(2).uppercase().required(),
+  })
+}), UserController.create);
 
 // route to list incidents
-routes.get('/incidents', IncidentController.index);
+routes.get('/incidents', celebrate({
+  [Segments.QUERY]: Joi.object().keys({
+    page: Joi.number(),
+  })
+}), IncidentController.index);
 
 // incident creation route
-routes.post('/incidents', IncidentController.create);
+routes.post('/incidents', celebrate({
+  [Segments.HEADERS]: Joi.object({
+    authorization: Joi.string().required().length(8),
+  }).unknown(),
+  [Segments.BODY]: Joi.object().keys({
+    number: Joi.number().required(),
+    title: Joi.string().required(),
+    description: Joi.string().required(),
+    client: Joi.string().required(),
+    date: Joi.string().required(),
+  })
+}), IncidentController.create);
 
 // route to exclude incidents
-routes.delete('/incidents/:id', IncidentController.delete);
+routes.delete('/incidents/:id', celebrate({
+  [Segments.PARAMS]: Joi.object().keys({
+    id: Joi.number().required(),
+  }) 
+}), IncidentController.delete);
 
 // route to list incidents for a specific user
-routes.get('/profile', ProfileController.index);
+routes.get('/profile', celebrate({
+  [Segments.HEADERS]: Joi.object({
+    authorization: Joi.string().required().length(8),
+  }).unknown(),
+}), ProfileController.index);
 
 module.exports = routes;
